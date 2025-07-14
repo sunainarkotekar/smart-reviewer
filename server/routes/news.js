bd86d02e351172d52c8d58bce007c68a1b65fa1d
sunainarkotekar <sunainarkotekar@gmail.com>
2025-07-14 10:50:32 +0100

    cleanup

--- server/routes/news.js
+++ server/routes/news.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const router = express.Router();
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   const query = req.query.q || "technology";
   console.log(`Fetching news for query: ${query}`);
   try {
     const url = `https://gnews.io/api/v4/search?q=${query}&lang=en&apikey=${process.env.GNEWS_API_KEY}`;
-    console.log("GNews API URL:", url);
+    // console.log("GNews API URL:", url);
     const response = await axios.get(url);
-    console.log("✅ GNews Response:", response.data);
+    // console.log("✅ GNews Response:", response.data);
     res.json(response.data);
   } catch (err) {
     console.error("❌ GNews API error:", err.response?.data || err.message);
-    res.status(500).json({ error: 'News API error' });
+    res.status(500).json({ error: "News API error" });
   }
 });
 
